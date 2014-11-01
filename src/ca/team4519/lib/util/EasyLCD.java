406af11330d3711fc1d0fc83e0907d019af35aaa
frc4519 <team4519@kingschristian.net>
2014-11-01 01:12:10 -0400

    Began EasyLCD interface for the DSLCD

---
+++ src/ca/team4519/lib/util/EasyLCD.java
@@ -0,0 +1,17 @@
+ 
+package ca.team4519.lib.util;
+
+/**
+ *
+ * @author Connor Adams
+ */
+public class EasyLCD {
+
+    private static final EasyLCD instnace = new EasyLCD();
+    private static final int DSTimeout = 20;
+    
+    private EasyLCD(){
+        
+    }
+    
+}

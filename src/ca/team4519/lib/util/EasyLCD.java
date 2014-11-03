6f0ded3a44f6c374e9fae824fa36c9e9947e06dd
Kings Mechavaliers <frc4519@users.noreply.github.com>
2014-11-03 01:48:16 -0500

    Update EasyLCD.java

--- src/ca/team4519/lib/util/EasyLCD.java
+++ src/ca/team4519/lib/util/EasyLCD.java
@@ -8,7 +8,6 @@ package ca.team4519.lib.util;
 public class EasyLCD {
 
     private static final EasyLCD instnace = new EasyLCD();
-    private static final int DSTimeout = 20;
     
     private EasyLCD(){
         
